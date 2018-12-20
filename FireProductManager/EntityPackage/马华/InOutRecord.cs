913b38b4523d116af099d361c1e958912beddc38
admin <admin@DESKTOP-OSRP7ML>
2018-12-20 11:09:22 +0800

    2018-12-20 修改数据库

--- FireProductManager/EntityPackage/马华/InOutRecord.cs
+++ FireProductManager/EntityPackage/马华/InOutRecord.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DbLink;
+﻿using DbLink;
+using System;
 
 namespace FireProductManager.EntityPackage
 {
